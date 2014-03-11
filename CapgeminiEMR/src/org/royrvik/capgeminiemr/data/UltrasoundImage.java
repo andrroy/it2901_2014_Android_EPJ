9f1bd055c593bf779215b71fa15a0d1ba63fa474
mgnusl <mgnusl@gmail.com>
2014-03-11 18:08:09 +0100

    Add UltrasoundImage class

---
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/data/UltrasoundImage.java
@@ -0,0 +1,28 @@
+package org.royrvik.capgeminiemr.data;
+
+public class UltrasoundImage {
+
+    private String imageUri;
+    private String comment;
+
+    public UltrasoundImage(String imageUri, String comment) {
+        this.imageUri = imageUri;
+        this.comment = comment;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
