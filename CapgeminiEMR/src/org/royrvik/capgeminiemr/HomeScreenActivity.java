1d601e71d15c932b585ab899cb5ba3ac8f0210a8
Laxcor <plaxcikus@gmail.com>
2014-03-17 12:57:45 +0100

    hack

--- CapgeminiEMR/src/org/royrvik/capgeminiemr/HomeScreenActivity.java
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/HomeScreenActivity.java
@@ -25,7 +25,7 @@ public class HomeScreenActivity extends SherlockActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
-
+//
         context = this;
 
         //Actionbarsherlock back button
