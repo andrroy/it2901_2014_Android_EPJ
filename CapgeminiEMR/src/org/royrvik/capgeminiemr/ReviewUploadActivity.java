fda29c70fe593379e36d0bef27dd3b24d3dc1c8b
mgnusl <mgnusl@gmail.com>
2014-03-18 23:07:59 +0100

    Work on Reviewupload

--- CapgeminiEMR/src/org/royrvik/capgeminiemr/ReviewUploadActivity.java
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/ReviewUploadActivity.java
@@ -2,18 +2,16 @@ package org.royrvik.capgeminiemr;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
-import de.keyboardsurfer.android.widget.crouton.Crouton;
-import de.keyboardsurfer.android.widget.crouton.Style;
+import android.widget.TextView;
 import org.royrvik.capgeminiemr.adapter.ReviewListAdapter;
-import org.royrvik.capgeminiemr.data.Examination;
 import org.royrvik.capgeminiemr.data.UltrasoundImage;
 import org.royrvik.capgeminiemr.database.DatabaseHelper;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -24,6 +22,9 @@ public class ReviewUploadActivity extends Activity {
     private ListView reviewListView;
     private Context context;
 
+    private Button editButton, uploadButton;
+    private TextView reviewIdTextView, reviewNameTextView;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reviewupload);
@@ -32,10 +33,33 @@ public class ReviewUploadActivity extends Activity {
 
         context = this;
 
+        // Fetch examination from database and show its images and comments in the listview
         List<UltrasoundImage> examinationImages = dbHelper.getExamination(1).getUltrasoundImages();
-
         reviewListView = (ListView) findViewById(R.id.reviewListView);
         reviewListView.setAdapter(new ReviewListAdapter(context, R.layout.row_list_item_review, examinationImages));
+
+        // Buttons
+        editButton = (Button) findViewById(R.id.editButton);
+        uploadButton = (Button) findViewById(R.id.uploadButton);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("APP", "Do something...");
+            }
+        });
+
+        // Textviews
+        reviewIdTextView = (TextView) findViewById(R.id.reviewIdTextView);
+        reviewIdTextView.setText("ID: " + dbHelper.getExamination(1).getPatientSsn());
+        reviewNameTextView = (TextView) findViewById(R.id.reviewNameTextView);
+        reviewNameTextView.setText("Name: " + dbHelper.getExamination(1).getPatientName());
+
 
     }
 }
