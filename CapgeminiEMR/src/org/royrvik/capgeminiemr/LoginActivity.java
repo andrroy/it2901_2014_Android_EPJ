8832309e33e62c81a06f5a56295e23593c702b03
mgnusl <mgnusl@gmail.com>
2014-03-18 22:43:44 +0100

    Add NetworkChecker class for checking the status of internet connection. Add ACCESS_NETWORK_STATE to Manifest

--- CapgeminiEMR/src/org/royrvik/capgeminiemr/LoginActivity.java
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/LoginActivity.java
@@ -2,12 +2,14 @@ package org.royrvik.capgeminiemr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.actionbarsherlock.app.SherlockActivity;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
+import org.royrvik.capgeminiemr.utils.NetworkChecker;
 
 import java.util.ArrayList;
 
---
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/utils/NetworkChecker.java
@@ -0,0 +1,30 @@
+package org.royrvik.capgeminiemr.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkChecker {
+
+    /**
+     *
+     * @param context
+     * @return true if the device is connected to the internet, false if it isn't
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        boolean isConnectedWifi = false;
+        boolean isConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] networkInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo n : networkInfo) {
+            if (n.getTypeName().equalsIgnoreCase("WIFI"))
+                if (n.isConnected())
+                    isConnectedWifi = true;
+            if (n.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (n.isConnected())
+                    isConnectedMobile = true;
+        }
+        return isConnectedWifi || isConnectedMobile;
+    }
+}
