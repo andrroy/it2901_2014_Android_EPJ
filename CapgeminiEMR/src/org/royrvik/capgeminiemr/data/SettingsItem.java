6d4485811a5e5a284393f271d5b8d09bd5213668
Magnus Lund <mgnusl@gmail.com>
2014-03-26 12:34:12 +0100

    Add SettingsItem class for easier display of settings data

---
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/data/SettingsItem.java
@@ -0,0 +1,27 @@
+package org.royrvik.capgeminiemr.data;
+
+public class SettingsItem {
+
+    private String key, value;
+
+    public SettingsItem(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
