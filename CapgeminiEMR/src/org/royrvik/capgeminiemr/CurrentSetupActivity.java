d8177e727e126a4f238d52c2e8de13c000c42a53
Magnus Lund <mgnusl@gmail.com>
2014-03-26 12:36:37 +0100

    CurrentSetupActivity now showing the current setup

--- CapgeminiEMR/src/org/royrvik/capgeminiemr/CurrentSetupActivity.java
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/CurrentSetupActivity.java
@@ -2,16 +2,13 @@ package org.royrvik.capgeminiemr;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ListView;
 import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.app.SherlockListActivity;
 import com.actionbarsherlock.view.MenuItem;
 import org.royrvik.capgeminiemr.adapter.CurrentSetupListAdapter;
-import org.royrvik.capgeminiemr.database.DatabaseHelper;
+import org.royrvik.capgeminiemr.data.SettingsItem;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 public class CurrentSetupActivity extends SherlockActivity {
@@ -33,29 +30,28 @@ public class CurrentSetupActivity extends SherlockActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
         // Get current settings from Application class
-        ArrayList<ArrayList<String>> keyValueList = buildData(globalApp.getAllPreferences());
+        ArrayList<SettingsItem> listOfSettings = buildData(globalApp.getAllPreferences());
 
         currentSetupListView = (ListView) findViewById(R.id.currentSetupListView);
-        currentSetupListView.setAdapter(new CurrentSetupListAdapter(context, R.layout.row_list_item_currentsetup, keyValueList));
-
+        currentSetupListView.setAdapter(new CurrentSetupListAdapter(context, R.layout.row_list_item_currentsetup, listOfSettings));
 
     }
 
-    public ArrayList<ArrayList<String>> buildData(Map<String, ?> preferenceHashMap) {
+    /**
+     * Builds the dataset for the adapter to use
+     *
+     * @param preferenceHashMap Hashmap from EMRApplication
+     * @return ArrayList of all settings saved in SharedPreferences, each represented as SettingsItem
+     */
+    public ArrayList<SettingsItem> buildData(Map<String, ?> preferenceHashMap) {
 
-        ArrayList<String> keys = new ArrayList<String>();
-        ArrayList<String> values = new ArrayList<String>();
+        ArrayList<SettingsItem> settingsItemList = new ArrayList<SettingsItem>();
 
         for (String key : preferenceHashMap.keySet()) {
-            keys.add(key);
-            values.add((String) preferenceHashMap.get(key));
+            settingsItemList.add(new SettingsItem(key, (String) preferenceHashMap.get(key)));
         }
 
-        ArrayList<ArrayList<String>> keyValueList = new ArrayList<ArrayList<String>>();
-        keyValueList.add(keys);
-        keyValueList.add(values);
-
-        return keyValueList;
+        return settingsItemList;
     }
 
     @Override
--- CapgeminiEMR/src/org/royrvik/capgeminiemr/adapter/CurrentSetupListAdapter.java
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/adapter/CurrentSetupListAdapter.java
@@ -1,21 +1,18 @@
 package org.royrvik.capgeminiemr.adapter;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import org.royrvik.capgeminiemr.R;
-import org.royrvik.capgeminiemr.data.UltrasoundImage;
+import org.royrvik.capgeminiemr.data.SettingsItem;
 
 import java.util.ArrayList;
 
-public class CurrentSetupListAdapter extends ArrayAdapter<ArrayList<String>> {
+public class CurrentSetupListAdapter extends ArrayAdapter<SettingsItem> {
 
     private static final String TAG = "APP";
 
@@ -23,7 +20,7 @@ public class CurrentSetupListAdapter extends ArrayAdapter<ArrayList<String>> {
     private LayoutInflater inflater;
     private Context context;
 
-    public CurrentSetupListAdapter(Context context, int resourceId, ArrayList<ArrayList<String>> objects) {
+    public CurrentSetupListAdapter(Context context, int resourceId, ArrayList<SettingsItem> objects) {
         super(context, resourceId, objects);
         resource = resourceId;
         inflater = LayoutInflater.from(context);
@@ -36,13 +33,13 @@ public class CurrentSetupListAdapter extends ArrayAdapter<ArrayList<String>> {
         convertView = (RelativeLayout) inflater.inflate(resource, null);
 
         // Data for THIS row
-        ArrayList<String> rowItem = getItem(position);
+        SettingsItem rowItem = getItem(position);
 
         TextView keyTextView = (TextView) convertView.findViewById(R.id.currentSetupKeyTextView);
-        keyTextView.setText(rowItem.toString());
+        keyTextView.setText(rowItem.getKey());
 
         TextView valueTextView = (TextView) convertView.findViewById(R.id.currentSetupValueTextView);
-        valueTextView.setText(rowItem.toString());
+        valueTextView.setText(rowItem.getValue());
 
         return convertView;
 
