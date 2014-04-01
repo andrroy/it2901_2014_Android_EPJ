711aaf52adfffc934c020ada414077c880543f42
Magnus Lund <mgnusl@gmail.com>
2014-04-01 23:55:08 +0200

    Dialog responds to delete request

--- CapgeminiEMR/src/org/royrvik/capgeminiemr/HomeScreenActivity.java
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/HomeScreenActivity.java
@@ -2,13 +2,15 @@ package org.royrvik.capgeminiemr;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
 import org.royrvik.capgeminiemr.adapter.HomescreenListAdapter;
 import org.royrvik.capgeminiemr.data.Examination;
 import org.royrvik.capgeminiemr.database.DatabaseHelper;
@@ -45,10 +47,9 @@ public class HomeScreenActivity extends SherlockActivity {
         homescreenListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Examination ex = (Examination)parent.getItemAtPosition(position);
-                Log.d("APP", ex.getPatientName() + " " + ex.getPatientSsn());
+                final Examination ex = (Examination)parent.getItemAtPosition(position);
 
-                // Show popup
+                // Build and show popup
                 AlertDialog.Builder dialog = new AlertDialog.Builder(HomeScreenActivity.this);
                 dialog.setTitle("Examination ID " + ex.getId());
                 StringBuilder infoString = new StringBuilder();
@@ -58,10 +59,28 @@ public class HomeScreenActivity extends SherlockActivity {
                 infoString.append("Number of images: " + ex.getUltrasoundImages().size() + "\n");
 
                 dialog.setMessage(infoString);
+
                 dialog.setPositiveButton("OK", null);
-                dialog.setNegativeButton("Delete", null);
-                dialog.setNeutralButton("Edit", null);
+                dialog.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Delete the Examination from the database
+                        boolean deleted = dbHelper.deleteExamination(ex.getId());
+                        if(deleted)
+                            Crouton.makeText(HomeScreenActivity.this, "Examination deleted", Style.CONFIRM).show();
+                        else
+                            Crouton.makeText(HomeScreenActivity.this, "Examination not found in the database", Style.CONFIRM).show();
+                    }
+                });
+                dialog.setNeutralButton("Edit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Go
+                    }
+                });
                 dialog.show();
+
+
             }
         });
 
