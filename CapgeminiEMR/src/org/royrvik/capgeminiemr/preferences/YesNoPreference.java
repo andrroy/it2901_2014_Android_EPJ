f02f1ba1f2bf247529a0c4f53ba748cfbf17a073
mgnusl <mgnusl@gmail.com>
2014-04-04 00:38:10 +0200

    Delete data option in preferences should now clear database and sharedpreferences

--- CapgeminiEMR/src/org/royrvik/capgeminiemr/preferences/YesNoPreference.java
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/preferences/YesNoPreference.java
@@ -3,12 +3,38 @@ package org.royrvik.capgeminiemr.preferences;
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.util.Log;
+import org.royrvik.capgeminiemr.EMRApplication;
+import org.royrvik.capgeminiemr.database.DatabaseHelper;
 
 public class YesNoPreference extends DialogPreference {
+    private Context context;
+    private DatabaseHelper dbHelper;
+    private EMRApplication globalApp;
+
+
 
     public YesNoPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         // TODO Auto-generated constructor stub
+        this.context = context;
+        dbHelper = new DatabaseHelper(context);
+        globalApp = (EMRApplication) context.getApplicationContext();
+
     }
 
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (positiveResult) {
+            // Delete everything
+            Log.d("APP", "Slett alt");
+            dbHelper.deleteAllExaminations();
+            globalApp.clearSharedPreferences();
+
+        }
+        else {
+            Log.d("APP", "Slett ingenting");
+        }
+    }
 }
