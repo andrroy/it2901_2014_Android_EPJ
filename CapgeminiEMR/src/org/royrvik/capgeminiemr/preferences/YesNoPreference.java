5d9b4f17ab2c9b1461235039c191bd5d603850e7
mgnusl <mgnusl@gmail.com>
2014-04-03 18:39:58 +0200

    Add yes/no confirmation dialog for delete data preference

---
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/preferences/YesNoPreference.java
@@ -0,0 +1,14 @@
+package org.royrvik.capgeminiemr.preferences;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class YesNoPreference extends DialogPreference {
+
+    public YesNoPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+}
