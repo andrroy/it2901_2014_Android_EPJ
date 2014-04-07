5fb3e5f951454592ed21636a96f080c2ee6c392f
Magnus Lund <mgnusl@gmail.com>
2014-04-07 13:30:29 +0200

    Offline mode in homescreen

--- CapgeminiEMR/src/org/royrvik/capgeminiemr/HomeScreenActivity.java
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/HomeScreenActivity.java
@@ -52,65 +52,77 @@ public class HomeScreenActivity extends SherlockActivity {
         // Get all examinations in the database
         listOfExaminations = dbHelper.getAllExaminations();
 
-        homescreenListView = (ListView) findViewById(R.id.homeScreenListView);
-        final HomescreenListAdapter homescreenListAdapter = new HomescreenListAdapter(context, R.layout.row_list_item_homescreen, listOfExaminations);
-        homescreenListView.setAdapter(homescreenListAdapter);
-        homescreenListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                final Examination ex = (Examination)parent.getItemAtPosition(position);
+        if(!session.isValid()) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(HomeScreenActivity.this);
+            dialog.setMessage("This information is not available in offline mode");
+            dialog.setIcon(R.drawable.ic_info);
+            dialog.setNeutralButton("OK", null);
+            dialog.show();
+        }
+        else {
+            homescreenListView = (ListView) findViewById(R.id.homeScreenListView);
+            final HomescreenListAdapter homescreenListAdapter = new HomescreenListAdapter(context, R.layout.row_list_item_homescreen, listOfExaminations);
+            homescreenListView.setAdapter(homescreenListAdapter);
+            homescreenListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final Examination ex = (Examination)parent.getItemAtPosition(position);
 
-                // Build and show popup
-                AlertDialog.Builder dialog = new AlertDialog.Builder(HomeScreenActivity.this);
-                dialog.setTitle("Examination ID " + ex.getId());
-                StringBuilder infoString = new StringBuilder();
-                infoString.append("Name: " + ex.getPatientName() + "\n");
-                infoString.append("SSN: " + ex.getPatientSsn() + "\n");
-                infoString.append("Date: " + ex.getDate() + "\n");
-                infoString.append("Number of images: " + ex.getUltrasoundImages().size() + "\n");
+                    // Build and show popup
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(HomeScreenActivity.this);
+                    dialog.setTitle("Examination ID " + ex.getId());
+                    StringBuilder infoString = new StringBuilder();
+                    infoString.append("Name: " + ex.getPatientName() + "\n");
+                    infoString.append("SSN: " + ex.getPatientSsn() + "\n");
+                    infoString.append("Date: " + ex.getDate() + "\n");
+                    infoString.append("Number of images: " + ex.getUltrasoundImages().size() + "\n");
 
-                dialog.setMessage(infoString);
+                    dialog.setMessage(infoString);
+                    dialog.setIcon(R.drawable.ic_info);
 
-                dialog.setPositiveButton("OK", null);
-                dialog.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // Confirmation dialog
-                        AlertDialog.Builder confirmDialog = new AlertDialog.Builder(HomeScreenActivity.this);
-                        confirmDialog.setTitle("Confirm");
-                        confirmDialog.setMessage("Are you sure you want to delete this examination?");
-                        confirmDialog.setPositiveButton("Yes", null);
-                        confirmDialog.setNegativeButton("No", null);
-                        confirmDialog.show();
-                        // Delete the Examination from the database
-                        boolean deleted = dbHelper.deleteExamination(ex.getId());
+                    dialog.setPositiveButton("OK", null);
+                    dialog.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Confirmation dialog
+                            AlertDialog.Builder confirmDialog = new AlertDialog.Builder(HomeScreenActivity.this);
+                            confirmDialog.setTitle("Confirm");
+                            confirmDialog.setMessage("Are you sure you want to delete this examination?");
+                            confirmDialog.setPositiveButton("Yes", null);
+                            confirmDialog.setNegativeButton("No", null);
+                            confirmDialog.show();
+                            // Delete the Examination from the database
+                            boolean deleted = dbHelper.deleteExamination(ex.getId());
 
-                        if(deleted)
-                            Crouton.makeText(HomeScreenActivity.this, "Examination deleted", Style.CONFIRM).show();
-                        else
-                            Crouton.makeText(HomeScreenActivity.this, "Examination not found in the database", Style.ALERT).show();
+                            if(deleted)
+                                Crouton.makeText(HomeScreenActivity.this, "Examination deleted", Style.CONFIRM).show();
+                            else
+                                Crouton.makeText(HomeScreenActivity.this, "Examination not found in the database", Style.ALERT).show();
 
-                        // Update data set and notify the adapter of the changes
-                        listOfExaminations.clear();
-                        listOfExaminations.addAll(dbHelper.getAllExaminations());
-                        homescreenListAdapter.notifyDataSetChanged();
-                        homescreenListView.refreshDrawableState();
-                    }
-                });
-                dialog.setNeutralButton("Edit", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // Start ExaminationActivity
-                        Intent i = new Intent(HomeScreenActivity.this, ExaminationActivity.class);
-                        i.putExtra("ex_id", ex.getId());
-                        startActivity(i);
-                    }
-                });
-                dialog.show();
+                            // Update data set and notify the adapter of the changes
+                            listOfExaminations.clear();
+                            listOfExaminations.addAll(dbHelper.getAllExaminations());
+                            homescreenListAdapter.notifyDataSetChanged();
+                            homescreenListView.refreshDrawableState();
+                        }
+                    });
+                    dialog.setNeutralButton("Edit", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Start ExaminationActivity
+                            Intent i = new Intent(HomeScreenActivity.this, ExaminationActivity.class);
+                            i.putExtra("ex_id", ex.getId());
+                            startActivity(i);
+                        }
+                    });
+                    dialog.show();
 
 
-            }
-        });
+                }
+            });
+        }
+
+
 
     }
 
