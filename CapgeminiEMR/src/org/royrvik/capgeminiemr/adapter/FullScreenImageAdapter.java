dfa5215da3afe08e1790d9aab3610e12eeb4ab91
Magnus Lund <mgnusl@gmail.com>
2014-04-25 16:09:51 +0200

    Fullscreen image viewer deletes images

--- CapgeminiEMR/src/org/royrvik/capgeminiemr/adapter/FullScreenImageAdapter.java
+++ CapgeminiEMR/src/org/royrvik/capgeminiemr/adapter/FullScreenImageAdapter.java
@@ -1,34 +1,30 @@
 package org.royrvik.capgeminiemr.adapter;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.*;
-import android.app.Dialog;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.content.Context;
-import org.royrvik.capgeminiemr.R;
-
-import java.util.ArrayList;
-import android.view.LayoutInflater;
-
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.*;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.royrvik.capgeminiemr.R;
 import org.royrvik.capgeminiemr.data.Examination;
 import uk.co.senab.photoview.PhotoView;
 import uk.co.senab.photoview.PhotoViewAttacher;
-import android.view.ViewGroup.LayoutParams;
+
+import java.util.ArrayList;
 
 /**
  * Created by rikardeide on 20/4/14.
  */
 
-public class FullScreenImageAdapter extends PagerAdapter{
+public class FullScreenImageAdapter extends PagerAdapter {
 
     private ArrayList<String> imageURIs;
     private Examination currentExamination;
@@ -40,21 +36,22 @@ public class FullScreenImageAdapter extends PagerAdapter{
     private TextView text;
 
 
-    public FullScreenImageAdapter(Context context, Examination currentExamination){
+    public FullScreenImageAdapter(Context context, Examination currentExamination) {
         this.mContext = context;
         this.currentExamination = currentExamination;
         this.imageURIs = currentExamination.getAllImages();
+        Log.d("APP", "CALLED CONSTRUCTOR");
     }
 
-    public int getCount(){
-        return this.imageURIs.size();
+    public int getCount() {
+        return imageURIs.size();
     }
 
-    public boolean isViewFromObject(View view, Object object){
+    public boolean isViewFromObject(View view, Object object) {
         return view == object;
     }
 
-    public Object instantiateItem(final ViewGroup container, final int position){
+    public Object instantiateItem(final ViewGroup container, final int position) {
         final Button btnClose;
         final Button btnDelete;
         final Button btnComment;
@@ -74,23 +71,17 @@ public class FullScreenImageAdapter extends PagerAdapter{
 
         // TODO: Load a comprimized version of bitmap!
 
-        // Check dimentsions before decoding to avoid outOfMemoryException
-
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inPreferredConfig = Bitmap.Config.ARGB_8888;
 
         Bitmap bitmap = BitmapFactory.decodeFile(imageURIs.get(position), options);
-        String s = "??????: " + position;
-        Log.d("APP:", s);
 
         // photoView.setImageBitmap(bitmap);
         imageView.setImageBitmap(bitmap);
 
         mAttacher = new PhotoViewAttacher(imageView);
 
-
-
-        // *Button operations for close, delete and comment buttons*
+        // Button clicklisteners
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -104,7 +95,6 @@ public class FullScreenImageAdapter extends PagerAdapter{
                 btnComment.setBackgroundResource(R.drawable.ic_comment);
 
                 // Custom Dialog
-
                 final Dialog dialog = new Dialog(mContext);
                 dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -113,7 +103,7 @@ public class FullScreenImageAdapter extends PagerAdapter{
 
                 // set the custom dialog components - text and button
                 text = (TextView) dialog.findViewById(R.id.text);
-                if(!getText(position).equals(null)){
+                if (!getText(position).equals(null)) {
                     text.append(getText(0));
                 }
 
@@ -143,14 +133,12 @@ public class FullScreenImageAdapter extends PagerAdapter{
             }
         });
 
-        btnDelete.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 btnDelete.setBackgroundResource(R.drawable.ic_delete_up);
-                deleteImage();
-                destroyItem(container, 0, viewLayout);
-                notifyDataSetChanged();
+                Log.d("APP", "DELETE IMAGE POSITION " + position);
+                deleteImage(position);
             }
-
         });
 
         btnDelete.setOnTouchListener(new View.OnTouchListener() {
@@ -161,36 +149,43 @@ public class FullScreenImageAdapter extends PagerAdapter{
             }
         });
 
-
         container.addView(viewLayout);
         //container.addView(photoView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 
         return viewLayout;
     }
 
-    private String getText(int pos){
+    private String getText(int pos) {
         return currentExamination.getUltrasoundImages().get(pos).getComment();
     }
 
-    private void save(){
+    private void save() {
         //For testing purposes! It always comments the first image
         currentExamination.getUltrasoundImages().get(0).setComment(text.getText().toString());
     }
 
-    public void deleteImage(){
+    public void deleteImage(int index) {
         //TODO: Need to figure out how to identify the image being viewed.
-            // TODO: @dependency: what image is currently being viewed?
+        // TODO: @dependency: what image is currently being viewed?
 
-        if (currentExamination.getUltrasoundImages().size() <= 1) {
+        if (imageURIs.size() <= 1) {
             Log.d("APP:", "You can't delete your only image!");
         } else {
-            //For testing purposes! It always deletes the first image
-            currentExamination.deleteImage(0);
+            imageURIs.remove(index);
+            notifyDataSetChanged();
         }
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
-        ((ViewPager) container).removeView((View)object);
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
     }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+        Log.d("APP", "destroyItem() " + position);
+    }
+
+
 }
